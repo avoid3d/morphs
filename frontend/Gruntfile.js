e23a60639b0f5cd89f136f87c24a8e5f147da98d
Pierre Hugo <avoid3d@gmail.com>
2015-05-09 13:31:32 +0200

    Got angular and bootstrap working with grunt and bower.

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -1,7 +1,28 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-  });
+    grunt.initConfig({
+        uglify: {
+            options: {
+                mangle: false
+            },
+            morphs: {
+                files: {
+                    'morphs.min.js': []
+                }
+            }
+        },
+        concat: {
+            morphs: {
+                src: [
+                    'bower_components/jquery/dist/jquery.min.js',
+                    'bower_components/bootstrap/dist/js/bootstrap.min.js',
+                ],
+                dest: 'build/bower-dependencies.min.js',
+            },
+        },
+    });
 
-  grunt.registerTask('default', []);
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
+    grunt.registerTask('default', ['concat']);
 };
