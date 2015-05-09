68f3c26880f76b609b91841695f7a571514426b7
Pierre Hugo <avoid3d@gmail.com>
2015-05-09 14:09:39 +0200

    More python boilerplate and stubs.

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy
 
 def create_app(environment='dev'):
   app = Flask(__name__)
+
+  db.init_app(app)
 
   return app
--- backend/manage.py
+++ backend/manage.py
@@ -1,1 +1,12 @@
+import os, sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
+
+from flask.ext.script import Manager
+from backend import create_app
+
+app = create_app('dev')
+manager = Manager(app)
+
+if __name__ == "__main__":
+    manager.run()
